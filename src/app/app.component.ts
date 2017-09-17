132cff89854e64122e2a2fa7a57bb0eb65477663
Carlos I. Martinez <itscarlosisaac@gmail.com>
2017-09-17 17:40:56 -0400

    Added an attribute directive to the book component to check its type.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -144,15 +144,17 @@ export class AppComponent {
       author: 'Patrick Rothfuss',
       description: "En una posada en tierra de nadie, un hombre se dispone a relatar, por primera vez, la auténtica historia de su vida. Una historia que únicamente él conoce y que ha quedado diluida tras los rumores, las conjeturas y los cuentos de taberna que le han convertido en un personaje legendario a quien todos daban ya por muerto: Kvothe... músico, mendigo, ladrón, estudiante, mago, ...more",
       pages: 126,
-      genres: [ 'fantasy', 'fiction', 'epic-fantasy' ]
+      genres: [ 'fantasy', 'fiction', 'epic-fantasy' ],
+      type: 'long'
     },
     {
-      title: "El nombre del viento",
+      title: "Frankenstein",
       alias: '',
       author: 'Mary Wollstonecraft Shelley',
       description: "Mary Shelley began writing Frankenstein when she was only eighteen. At once a Gothic thriller, a passionate romance, and a cautionary tale about the dangers of science, Frankenstein tells the story of committed science student Victor Frankenstein. Obsessed with discovering the cause of generation and life and bestowing animation upon lifeless matter, Frankenstein assembles ...more",
       pages: 872,
-      genres: [ 'classic', 'fiction', 'horror', 'literature', 'novel' ]
+      genres: [ 'classic', 'fiction', 'horror', 'literature', 'novel' ],
+      type: 'short'
     },
   ]
 
